9892c0f7e7c7b01febae7d5ff1bdc3361a1a2a8b
balla2017 <31579914+balla2017@users.noreply.github.com>
2018-04-23 18:30:30 +0800

    Add files via upload

--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './navBar.css'
-import User from '../user/user.js'
+import User from '../user/User.js'
 import { NavLink } from 'react-router-dom'
 
 class NavBar extends Component {
   render() {
     return (
       <div id="navBar">
-        <User />
+        <NavLink
+          to="/user"
+          activeClassName="selected"
+        >
+         <User />
+        </NavLink>
+
         <NavLink
           to="/chatlist"
           activeClassName="selected"
@@ -22,10 +28,15 @@ class NavBar extends Component {
         <span className="iconfont icon-friends"></span>
         <span className="navBar-text">Contact List</span>
         </NavLink>
-      
-       <NavLink to="/login" activeClassName="selected">
-          <span className="navBar-text" id="logout">Log out</span>
-          </NavLink>
+        <NavLink
+          to="/Login"
+          activeClassName="selected"
+        >
+         <span className="navBar-text" id="logout" >Log out</span>
+        </NavLink>
+
+
+
       </div>
     );
   }
